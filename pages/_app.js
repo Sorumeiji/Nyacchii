6e21c8e68acd1110fe544a44d1c6b10c8618b4ed
Kaleb Berry <kaleb.berry@outlook.com>
2022-06-28 22:21:41 -0500

    wtf

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,20 @@ import '../styles/global.scss';
 import Layout from '../src/components/layout';
 import { useEffect } from 'react';
 import Head from 'next/head';
+import { AnimatePresence } from 'framer-motion';
 
 // This default export is required in a new `pages/_app.js` file.
-function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	});
+function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
 			<Head>
 				<title>NYACCHII - Full-Time Creative Streamer and Twitch Partner from Mexico.</title>
 			</Head>
+
 			<Layout>
-				<Component {...pageProps} />
+				<AnimatePresence exitBeforeEnter>
+					<Component {...pageProps} key={router.route} />
+				</AnimatePresence>
 			</Layout>
 		</>
 	);
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { faInstagram, faTwitch, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { motion, AnimatePresence, animate } from 'framer-motion';
 
 function Layout({ children }) {
+	const [words, setWords] = useState(true);
+
 	return (
 		<>
-			<div className='layout'>{children}</div>
+			<motion.div
+				key='modal'
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				transition={{ easeIn: [0.17, 0.67, 0.83, 0.67], duration: 1 }}>
+				{children}
+			</motion.div>
+
 			<footer className='footer'>
 				<div className='container flow'>
 					<ul className='social'>
