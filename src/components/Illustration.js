3339196494d76dabfcaecccdad2a56e288054280
Kaleb Berry <kaleb.berry@outlook.com>
2022-07-03 15:04:19 -0500

    updating links

--- src/components/Illustration.js
+++ src/components/Illustration.js
@@ -37,13 +37,13 @@ const Illustration = ({ pricing }) => {
 					<FsLightbox
 						toggler={toggler}
 						sources={[
-							'http://images.ctfassets.net/wpd16o1h1n58/17Ty2XrG1wn0No5EPeYrQ1/ddb4fe5a5cff735b914943cd3b1d4dab/01_72ppi_-_Watermark.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/14nNpDKrlGoSQUws4gXvZU/01205409dfefe57d81c0c428281ffde0/Doushin_002_72ppi_-_Watermark.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/2GkdeW5SFE48HVbC91HoZ9/2bf81ac869b38e295fa4bde9d60d9375/VENTI_002_POST.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/2koV2oPMdRhXSpHuFiEYEe/02f66bf301df96dd934a2e75ee07a9fa/HEXTURNAL_002_72ppi_-_Watermark.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/5wJuWLOFJPSwKp0WTMOuse/3f3860990d387b997fb1a1074c6d84dd/KITARG_001_72ppi_-_Watermark.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/4FgSbXKXg2P2fV7aNMe7Ew/7005347108e77fc8294e5c2e76160ff2/VENTI_002_72ppi_-_Watermark.png',
-							'http://images.ctfassets.net/wpd16o1h1n58/7tCC2m96lxjd5rU7iNhwce/7fa5b3aeee0412f456b15c732c3c9627/INOCHI_001_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/17Ty2XrG1wn0No5EPeYrQ1/ddb4fe5a5cff735b914943cd3b1d4dab/01_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/14nNpDKrlGoSQUws4gXvZU/01205409dfefe57d81c0c428281ffde0/Doushin_002_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/2GkdeW5SFE48HVbC91HoZ9/2bf81ac869b38e295fa4bde9d60d9375/VENTI_002_POST.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/2koV2oPMdRhXSpHuFiEYEe/02f66bf301df96dd934a2e75ee07a9fa/HEXTURNAL_002_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/5wJuWLOFJPSwKp0WTMOuse/3f3860990d387b997fb1a1074c6d84dd/KITARG_001_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/4FgSbXKXg2P2fV7aNMe7Ew/7005347108e77fc8294e5c2e76160ff2/VENTI_002_72ppi_-_Watermark.png',
+							'https://images.ctfassets.net/wpd16o1h1n58/7tCC2m96lxjd5rU7iNhwce/7fa5b3aeee0412f456b15c732c3c9627/INOCHI_001_72ppi_-_Watermark.png',
 						]}
 					/>
 				</div>
